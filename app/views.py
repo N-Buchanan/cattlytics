1a9b8df480a127b73868a7e1325b83a9297ea243
Nolan Buchanan <ncbuchan@gmail.com>
2017-03-05 13:01:43 -0500

    updated views
    lots of stuff, forgot what I did

--- app/views.py
+++ app/views.py
@@ -1,14 +1,26 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from .forms import EditAnimalForm, AddWeightForm
-from .models import Animal, Weight
+from .models import Animal, Weight, Medicine
 from .tables import AnimalTable, WeightTable
+import datetime
+from .algorithms.cow_math import calculate_weaning_weight
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    table = AnimalTable(Animal.query.all())
+    # table = AnimalTable(Animal.query.all())
+    animals = Animal.query.all()
+    for animal in animals:
+        animal.weaned = animal.has_weaned()
+
+    table = AnimalTable(animals)
     return render_template('index.html', title='Cattlytics', name='Test', table=table)
 
 
@@ -16,9 +28,37 @@ def index():
 def add_animal():
     form = EditAnimalForm()
     if form.validate_on_submit():
-        animal = Animal(primary_tag=form.primary_tag.data,
-                        birth_date=form.birth_date.data,
-                        birth_weight=form.birth_weight.data)
+        # animal = Animal(primary_tag=form.primary_tag.data,
+        #                 birth_date=form.birth_date.data,
+        #                 birth_weight=form.birth_weight.data)
+
+        animal = Animal()
+        # form.populate_obj(animal)
+        animal.primary_tag = form.primary_tag.data
+        animal.sex = form.sex.data
+        animal.birth_date = form.birth_date.data
+        animal.birth_weight = form.birth_weight.data
+        animal.sex = animal.sex[:1].upper()
+
+        # if the dam doesn't exist in the database, create it and get an id
+        if form.dam.data:  # but only if a dam was given
+            dam = Animal.query.filter_by(primary_tag=form.dam.data).first()
+            if not dam:
+                dam = Animal(primary_tag=form.dam.data)
+                db.session.add(dam)
+                db.session.commit()
+
+            animal.dam = dam.id
+
+        # same thing with the sire
+        if form.sire.data:
+            sire = Animal.query.filter_by(primary_tag=form.sire.data).first()
+            if not sire:
+                sire = Animal(primary_tag=form.sire.data)
+                db.session.add(sire)
+                db.session.commit()
+
+            animal.sire = sire.id
 
         db.session.add(animal)
         db.session.commit()
@@ -39,13 +79,12 @@ def add_animal():
 @app.route('/edit/<animal_id>', methods=['GET', 'POST'])
 def edit_animal(animal_id):
 
-    animal = db.session.query(Animal).filter_by(id=animal_id)[0]
+    animal = db.session.query(Animal).filter_by(id=animal_id).first()
     form = EditAnimalForm()
 
     if form.validate_on_submit():
-        animal = Animal.query.filter_by(id=form.id.data)[0]
+        animal = Animal.query.filter_by(id=form.id.data).first()
 
-        print(animal_id)
         # animal.birth_date = form.birth_date.data
         # animal.birth_weight = form.birth_weight.data
 
@@ -60,6 +99,16 @@ def edit_animal(animal_id):
         form.birth_date.data = animal.birth_date
         form.birth_weight.data = animal.birth_weight
         form.id.data = animal.id
+        if animal.dam:
+            dam = Animal.query.filter_by(id=animal.dam).first()
+            if dam:
+                print(dam)
+                form.dam.data = dam.primary_tag
+        if animal.sire:
+            sire = Animal.query.filter_by(id=animal.sire).first()
+            if sire:
+                form.sire.data = sire.primary_tag
+        form.sex.data = animal.sex
 
     weights = Weight.query.filter_by(animal_id=animal.id)
     weights_table = WeightTable(weights)
@@ -67,8 +116,15 @@ def edit_animal(animal_id):
     weight_form = AddWeightForm()
     weight_form.animal_id.data = animal.id
 
+    animal_age = (datetime.date.today() - animal.birth_date).days
+
+    adjusted_weight = None
+    if animal.has_weaned():
+        adjusted_weight = round(calculate_weaning_weight(animal))
+
     return render_template('edit.html', title='Cattlytics: Edit Animal', animal=animal.id,
-                           form=form, weights_table=weights_table, weight_form=weight_form)
+                           form=form, weights_table=weights_table, weight_form=weight_form,
+                           age=animal_age, adjusted_weight=adjusted_weight)
 
 
 @app.route('/add_weight', methods=['POST'])
@@ -85,6 +141,7 @@ def add_weight():
         weight.date = form.date.data
         weight.weight = form.weight.data
         weight.animal_id = form.animal_id.data
+        weight.weaning = form.weaning.data
 
         db.session.add(weight)
         db.session.commit()
@@ -93,3 +150,25 @@ def add_weight():
         return redirect(url_for('edit_animal', animal_id=form.animal_id.data))
 
     return redirect(url_for('edit_animal', animal_id=form.animal_id.data))
+
+
+@app.route('/delete_weight/<animal_id>/<date>', methods=['GET'])
+def delete_weight(animal_id, date):
+    weight = Weight.query.filter_by(animal_id=animal_id, date=date).first()
+
+    db.session.delete(weight)
+    db.session.commit()
+
+    return redirect(url_for('edit_animal', animal_id=animal_id))
+
+
+@app.route('/delete_animal/<animal_id>', methods=['GET'])
+def delete_animal(animal_id):
+
+    db.session.query(Animal).filter_by(id=animal_id).delete()
+    db.session.query(Weight).filter_by(animal_id=animal_id).delete()
+    db.session.query(Medicine).filter_by(animal_id=animal_id).delete()
+
+    db.session.commit()
+
+    return redirect(url_for('index'))
